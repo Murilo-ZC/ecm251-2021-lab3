0f8915382b8659aafbb4ab1ee5b730d6165d0b4f
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-15 12:03:18 -0300

    Ajustes do Sistema

--- Aula05/src/carvalho/zanini/Sistema.java
+++ Aula05/src/carvalho/zanini/Sistema.java
@@ -1,7 +1,30 @@
 package carvalho.zanini;
+import java.util.Scanner;
 
 public class Sistema {
+    private boolean continuarExecucao;
+    private Scanner scanner;
+
+    public Sistema() {
+        this.continuarExecucao = true;
+        this.scanner = new Scanner(System.in);
+    }
+
     public void executar(){
-        System.out.println("Aqui!");
+        while(continuarExecucao) {
+            exibirMenu();
+            int opcao = scanner.nextInt();
+        }
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar título");
+        System.out.println("0 - Sair do sistema");
+
     }
 }
