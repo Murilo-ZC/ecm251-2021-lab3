fb283a4788d6b81b75e493b177f6067f108453da
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-05-24 13:15:37 -0300

    Collections

---
+++ Aula 12/src/br/maua/linked_list_test_drive/LinkedListTestDrive.java
@@ -0,0 +1,27 @@
+package br.maua.linked_list_test_drive;
+
+import br.maua.model.Pessoa;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListTestDrive {
+    public static void main(String[] args) {
+        LinkedList<Pessoa> pessoas = new LinkedList<>();
+        pessoas.add(new Pessoa("Batman", "001"));
+        pessoas.add(new Pessoa("Superman", "002"));
+        pessoas.add(new Pessoa("Mutano", "056"));
+        pessoas.addFirst(new Pessoa("Ravena", "000"));
+
+        System.out.println("=============");
+        System.out.println("Passando por todo a ListaLigada:");
+        pessoas.forEach(pessoa -> System.out.println(pessoa));
+
+        System.out.println("=============");
+        System.out.println("Passando por todo a ListaLigada Iterator:");
+        Iterator<Pessoa> iterator = pessoas.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
